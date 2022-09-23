676ee28a763843eb1ca3b55a9cea6da8dbbf4196
02mine03cfrat <114260523+02mine03cfrat@users.noreply.github.com>
2022-09-23 17:40:09 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+let P2 = 0
+let P1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("Simbora!")
+    basic.pause(randint(1000, 5000))
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    P2 += 1
+    basic.showString("2")
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    P1 += 1
+    basic.showString("1")
+    basic.pause(1000)
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
